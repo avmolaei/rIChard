3fdf1efabc4c5ac73146b3b49261c7ade8595442
Avesta Molaei <avmolaei@gmail.com>
2021-12-17 02:49:52 +0100

    finally repaired

--- richard_bt.ino
+++ richard_bt.ino
@@ -7,10 +7,13 @@ int out4 = 10;
 int out5 = 11;
 int out6 = 12; 
 
+
+String cmd1;
+String cmd2;
+String cmd3;
+String cmd4;
+
 String cmd = "";
-String cmdWord;
-String cmdRoom;
-String cmdDevice;
 SoftwareSerial bt(2, 3);
 void setup() {
   bt.begin(9600);
@@ -21,80 +24,68 @@ void setup() {
   pinMode(out4, OUTPUT);
   pinMode(out5, OUTPUT);
   pinMode(out6, OUTPUT);
-  Serial.println("démarrage...");
+  Serial.println("\n\n--------------------\ndémarrage...");
 }
 
 void loop() {
   while (bt.available() > 0) {
     cmd = bt.readString();
-    Serial.println("Recu: " + cmd + "\n");
+    cmd.replace(" ",";");
+    cmd.replace("\n", ";");
+    
+    cmd1 = cmd.substring(0, 8);
+    cmd2 = cmd.substring(10, 18);
+    cmd3 = cmd.substring(20, 28);
+    cmd4 = cmd.substring(30, 38);
+    Serial.print("\nBT RX: "+cmd);
     if (cmd != "") {
-      Serial.println("Recu: " + cmd + "\n");
-      cmdWord = cmd.substring(0, 2);
-      cmdRoom = cmd.substring(3, 8);
-      cmdDevice = cmd.substring(9);
-      Serial.println(cmdWord+"\n"+cmdRoom+"\n"+cmdDevice+"\n");
+     // Serial.println(cmdWord+"\n"+cmdRoom+"\n"+cmdDevice+"\n");
 
-      if(cmdRoom.equals("salon")){
-        if(cmdDevice.equals("1")){//lampe
-          if(cmdWord.equals("on")){
-            digitalWrite(out1, HIGH);
-            Serial.println("on, salon, 1");
-          }
-          else if(cmdWord.equals("of")){
-            digitalWrite(out1   , LOW);
-          }
-        }
-        if(cmdDevice.equals("2")){//ordi
-          if(cmdWord.equals("on")){
-            digitalWrite(out4, HIGH);
-            Serial.println("on, salon, 2");
-          }
-          else if(cmdWord.equals("of")){
-            digitalWrite(out4, LOW);
-          }
-        }
-        if(cmdDevice.equals("3")){//pc
-          if(cmdWord.equals("on")){
-            digitalWrite(out3, HIGH);
-            Serial.println("on, salon, 3");
-          }
-          else if(cmdWord.equals("of")){
-            digitalWrite(out3, LOW);
-          }
-        }
-      }
-      if(cmdRoom.equals("chamb")){
-        if(cmdDevice.equals("1")){//lampe
-          if(cmdWord.equals("on")){
-            digitalWrite(out5, HIGH);
-            Serial.println("on, chamb, 1");
-          }
-          else if(cmdWord.equals("of")){
-            digitalWrite(out5, LOW);
-          }
-        }
-        if(cmdDevice.equals("2")){//ordi
-          if(cmdWord.equals("on")){
-            digitalWrite(out2, HIGH);
-            Serial.println("on, chamb, 2");
-          }  
-          else if(cmdWord.equals("of")){
-            digitalWrite(out2, LOW);
-          } 
-        }
-        if(cmdDevice.equals("3")){//pc
-          if(cmdWord.equals("on")){
-            digitalWrite(out6, HIGH);
-            Serial.println("on, chamb, 3");
-          }
-          else if(cmdWord.equals("of")){
-            digitalWrite(out6, LOW);
-          }
-        }
-      }
+Serial.println("\n\nSUM(cmd): "+cmd1+cmd2+cmd3+cmd4);
+    if(cmd1.equals("chamb1ON")){
+      digitalWrite(out1, HIGH);
+      Serial.println(cmd1+": chambre 1 allumée\n");
+    }
+     if(cmd1.equals("chamb1OF")){
+      digitalWrite(out1, LOW);
+      Serial.println(cmd1+": chambre 1 éteinte\n");
+    }
+
+    if(cmd2.equals("chamb2ON")){
+      digitalWrite(out2, HIGH);
+      Serial.println(cmd2+": chambre 2 allumée\n");
+    }
+     if(cmd2.equals("chamb2OF")){
+      digitalWrite(out2, LOW);
+      
+      Serial.println(cmd2+": chambre 2 éteinte\n");
+    }
+
+
+    if(cmd3.equals("salon1ON")){
+      digitalWrite(out3, HIGH);
+      Serial.println(cmd3+": salon 1 allumée\n");
+    }
+     if(cmd3.equals("salon1OF")){
+      digitalWrite(out3, LOW);
+      Serial.println(cmd3+": salon 1 éteinte\n");
+    }
+
+    if(cmd4.equals("salon2ON")){
+      digitalWrite(out4, HIGH);
+      Serial.println(cmd4+": salon 2 allumée\n");
+    }
+     if(cmd4.equals("salon2OF")){
+      digitalWrite(out4, LOW);
+      Serial.println(cmd4+": salon 2 éteinte\n");
+    }
+
+    
+
+
 
       
+
     }
-  }  
-}
+    }
+    }
